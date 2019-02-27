84973e906ac7ea9ffd47ec73ae4df09934b970f7
Ryan Nicolay <ryannicolay@gmail.com>
2019-02-27 08:03:50 -0500

    Initial commit of word beater game

---
+++ js/main.js
@@ -0,0 +1,52 @@
+window.addEventListener("load", init);
+
+// Globals
+let time = 3;
+let score = 0;
+let isPlaying;
+
+// DOM Elements
+const wordInput = document.querySelector("#word-input");
+const currentWord = document.querySelector("#current-word");
+const scoreDisplay = document.querySelector("#socre");
+const timeDisplay = document.querySelector("#time");
+const message = document.querySelector("#message");
+const seconds = document.querySelector("#seconds");
+
+const words = [
+  "hat",
+  "river",
+  "lucky",
+  "statue",
+  "generate",
+  "stubborn",
+  "cocktail",
+  "runaway",
+  "joke",
+  "developer",
+  "establishment",
+  "hero",
+  "javascript",
+  "nutrition",
+  "revolver",
+  "echo",
+  "siblings",
+  "investigate",
+  "horrendous",
+  "symptom",
+  "laughter",
+  "magic",
+  "master",
+  "space",
+  "definition"
+];
+
+// Initialize Game
+function init() {
+  showWord(words);
+}
+
+function showWord(words) {
+  const randIndex = Math.floor(Math.random() * words.length);
+  currentWord.innerHTML = words[randIndex];
+}
